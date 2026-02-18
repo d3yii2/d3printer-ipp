57164f9e4e1be5d1b9e3e5dc2cb524f313acfe30
uldis <uldis@weberp.lv>
2026-02-18 12:00:09 +0200

    Refactored `PrintSpooler` to use class properties for storing temporary data, improving encapsulation. Updated `SpoolerCommand` to include exit codes and refined initialization for enhanced daemon control.

--- commands/SpoolerCommand.php
+++ commands/SpoolerCommand.php
@@ -2,21 +2,23 @@
 
 namespace d3yii2\d3printeripp\commands;
 
-
 use d3system\commands\DaemonController;
 use d3yii2\d3printeripp\components\PrintSpooler;
 use Yii;
 use yii\base\InvalidConfigException;
+use yii\console\ExitCode;
 use yii\db\Exception;
 
 class SpoolerCommand extends DaemonController
 {
 
-    public function init()
+    public function init(): void
     {
         $this->monoLogFileName = 'printer-spooler';
         $this->monoLogName = 'printer-spooler';
         $this->monoLogRuntimeDirectory = 'logging/printer-spooler';
+        $this->sleepAfterMicroseconds = 1000000; //1 second
+        $this->loopExitAfterSeconds = 60 * 60; // 1 hour
         parent::init();
     }
 
@@ -28,15 +30,16 @@ class SpoolerCommand extends DaemonController
         string $spoolerComponentName,
         ?string $printerComponentNamesCsv = null
     ): int {
-
         if (!Yii::$app->has($spoolerComponentName)) {
             $errorMessage = 'Not found spooler component with name: "' . $spoolerComponentName . '"';
             Yii::error($errorMessage);
+            return ExitCode::CONFIG;
         }
         /** @var PrintSpooler $spooler */
         if (!$spooler = Yii::$app->get($spoolerComponentName)) {
             $errorMessage = 'Not found spooler component with name: "' . $spoolerComponentName . '"';
             Yii::error($errorMessage);
+            return ExitCode::CONFIG;
         }
         $spooler->outCallback = function ($msg) {
             $this->out($msg);
@@ -48,6 +51,6 @@ class SpoolerCommand extends DaemonController
         while ($this->loop()) {
             $spooler->run();
         }
+        return ExitCode::OK;
     }
-
 }
--- components/PrintSpooler.php
+++ components/PrintSpooler.php
@@ -17,18 +17,24 @@ class PrintSpooler extends Component
      */
     public $outCallback;
 
+    /** @var BasePrinter|string[] $printers */
+    private $printers = [];
+
+    private array $printerProblems = [];
+    private array $printerFilesProblems = [];
+    private array $canNotUnlinkFiles = [];
+    
     public function run(): void
     {
-        /** @var BasePrinter|string[] $printers */
-        $printers = [];
-        $printerProblems = [];
-        $printerFilesProblems = [];
-        $canNotUnlinkFiles = [];
+        $this->printers = [];
+        $this->printerProblems = [];
+        $this->printerFilesProblems = [];
+        $this->canNotUnlinkFiles = [];
         foreach ($this->printersComponentNames as $printerComponentName) {
             /**
              * load printer, if not loaded yet
              */
-            if (!isset($printers[$printerComponentName])) {
+            if (!isset($this->printers[$printerComponentName])) {
                 $printer = false;
                 try {
                     if (!Yii::$app->has($printerComponentName)) {
@@ -48,14 +54,14 @@ class PrintSpooler extends Component
                         'msg' => $msg
                     ]);
                 }
-                $printers[$printerComponentName] = $printer;
+                $this->printers[$printerComponentName] = $printer;
             }
 
             /**
              * if printer no loaded, ignore it
             * @var BasePrinter|null $printer
             */
-            if (!$printer = $printers[$printerComponentName] ?? null) {
+            if (!$printer = $this->printers[$printerComponentName] ?? null) {
                 continue;
             }
 
@@ -68,33 +74,39 @@ class PrintSpooler extends Component
                 $msg = 'Try to get spool directory files.'
                     . ' Printer component: ' . $printerComponentName;
                 $fullMsg = $msg . PHP_EOL . $e->getMessage();
-                if (($printerProblems[$printerComponentName] ?? false) !== $fullMsg) {
+                if (($this->printerProblems[$printerComponentName] ?? false) !== $fullMsg) {
+                    $this->out($fullMsg);
                     Yii::error([
                         'exception' => $e,
                         'msg' => $msg
                     ]);
-                    $printerProblems[$printerComponentName] = $fullMsg;
+                    $this->printerProblems[$printerComponentName] = $fullMsg;
                 }
                 continue;
             }
             foreach ($spoolDirectoryFiles as $file) {
-                if (in_array($file, $canNotUnlinkFiles, true)) {
+                if (in_array($file, $this->canNotUnlinkFiles, true)) {
                     continue;
                 }
                 try {
                     $this->out($printerComponentName . ': Print file: ' . $file);
                     $printer->printFile($file);
                 } catch (Exception $e) {
+                    $exceptionMessage = $e->getMessage();
+                    if (strpos($exceptionMessage, 'Error: server-error-busy') !== false) {
+                        $this->out('printer busy');
+                        break;
+                    }
                     $msg = 'Try print file.'
                         . ' Printer component: ' . $printerComponentName
                         . ' File: ' . $file;
-                    if (!($printerFilesProblems[$printerComponentName][$file] ?? false)) {
-                        $this->out($msg);
+                    if (!($this->printerFilesProblems[$printerComponentName][$file] ?? false)) {
+                        $this->out($msg . ' Error: ' .  $exceptionMessage);
                         Yii::error([
                             'exception' => $e,
                             'msg' => $msg
                         ]);
-                        $printerFilesProblems[$printerComponentName][$file] = $e->getMessage();
+                        $this->printerFilesProblems[$printerComponentName][$file] = $exceptionMessage;
                     } else {
                         $this->out('E');
                     }
@@ -102,17 +114,17 @@ class PrintSpooler extends Component
                 }
                 try {
                     if (!$printer->deleteSpoolDirectoryFile($file)
-                        && !in_array($file, $canNotUnlinkFiles, true)
+                        && !in_array($file, $this->canNotUnlinkFiles, true)
                     ) {
-                        $canNotUnlinkFiles[] = $file;
+                        $this->canNotUnlinkFiles[] = $file;
                         $msg = 'Can not delete file: '
                             . ' Printer component: ' . $printerComponentName
                             . ' File: ' . $file;
                         Yii::error($msg);
                     }
                 } catch (Exception $e) {
-                    if (!in_array($file, $canNotUnlinkFiles, true)) {
-                        $canNotUnlinkFiles[] = $file;
+                    if (!in_array($file, $this->canNotUnlinkFiles, true)) {
+                        $this->canNotUnlinkFiles[] = $file;
                         $msg = ' Can not delete file.'
                             . ' Printer component: ' . $printerComponentName
                             . ' File: ' . $file;
